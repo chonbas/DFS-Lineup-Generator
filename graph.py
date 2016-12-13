3a4cffd767c57f6b465846bbe269556d86b2e2ea
ifosli <ifosli@stanford.edu>
2016-12-12 20:36:19 -0800

    finished code for adding graphs with current data

--- graph.py
+++ graph.py
@@ -1,14 +1,54 @@
+import csv
 import matplotlib.pyplot as plt
 from matplotlib.mlab import csv2rec
 from matplotlib.cbook import get_sample_data
 
 
+POSITIONS = ['QB', 'WR', 'RB', 'TE', 'PK', 'Def']
+ALGORITHMS = ['RF', 'LReg']
+TYPES = ['regression']
+WEEKS = 12
+
+LABELS = {'RF': 'Random Forest ', 'LReg': 'Linear '}
+
+def drawMeanSquaresError():
+    yPlotRange = range(20,71,10)
+    yMin = 12
+    yMax = 73
+    drawErrorLineGraph(yPlotRange, yMin, yMax, "Graphs/mean_squares_error.png", "Mean Squares Error", 2)
+
+def drawAccuracyGraph():
+    yPlotRange = [i/10.0 for i in range(3, 8)]
+    yMin = .25
+    yMax = .75
+    drawErrorLineGraph(yPlotRange, yMin, yMax, "Graphs/accuracy_score.png", "Accuracy Score", -1)
+
+def drawErrorLineGraph(yPlotRange, yMin, yMax, outputFile, title, param):
+    data = {}
+    categories = []
+    for alg in ALGORITHMS:
+        info = []
+        for t in TYPES:
+            with open('Evaluations/' + t + '_' + alg + '_eval.csv', 'r') as infile:
+                infile.next()
+                reader = csv.reader(infile, delimiter=',', quotechar='"')
+                for line in reader:
+                    pos = line[0]
+                    val = float(line[param]) # -1: accuracy, 2: mean squares
+                    info.append(float(val)) #assume position in correct order
+        data[(alg, t)] = info
+        categories.append((alg, t))
+    drawLineGraph(data, xAxis='positions', xLabels=POSITIONS, categories=categories, \
+        title=title, outputFile=outputFile, yMin=yMin, yMax=yMax, yPlotRange=yPlotRange)
+
+
+
 
 # Assume data formatted as data[category] = list of length numWeeks, for each
-# category in categories and yAxis.
+# category in categories and xAxis.
 
-def drawLineGraph(data, yAxis='week', categories=['data'], numWeeks=13, xLabel='week', yLabel='y-axis', \
-    title='title', outputFile='output.png'):
+def drawLineGraph(data, xAxis='week', xLabels=range(1,13), categories=['data'], \
+    title='title', outputFile='output.png', yMin=7, yMax=72, yPlotRange=range(10,71,10)):
 
     # fname = get_sample_data('percent_bachelors_degrees_women_usa.csv')
     # gender_degree_data = csv2rec(fname)
@@ -32,31 +72,30 @@ def drawLineGraph(data, yAxis='week', categories=['data'], numWeeks=13, xLabel='
 
     # Ensure that the axis ticks only show up on the bottom and left of the plot.
     # Ticks on the right and top of the plot are generally unnecessary.
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
+    # ax.get_xaxis().tick_bottom()
+    # ax.get_xaxis().tick_left()
 
     # Limit the range of the plot to only where the data is.
     # Avoid unnecessary whitespace.
-    # plt.xlim(1968.5, 2011.1)
-    # plt.ylim(-0.25, 90)
+    plt.xlim(-.5, len(xLabels) - .5)
+    plt.ylim(yMin,yMax)
 
     # Make sure your axis ticks are large enough to be easily read.
     # You don't want your viewers squinting to read your plot.
-    # plt.xticks(range(1970, 2011, 10), fontsize=14)
-    # plt.yticks(range(0, 91, 10), ['{0}%'.format(x)
-    #                               for x in range(0, 91, 10)], fontsize=14)
+    plt.xticks(range(len(xLabels)), xLabels, fontsize=14)
+    plt.yticks(yPlotRange, fontsize=14)
 
     # Provide tick lines across the plot to help your viewers trace along
     # the axis ticks. Make sure that the lines are light and small so they
     # don't obscure the primary data lines.
-    # for y in range(10, 91, 10):
-    #     plt.plot(range(1969, 2012), [y] * len(range(1969, 2012)), '--',
-    #              lw=0.5, color='black', alpha=0.3)
+    for y in yPlotRange:
+        plt.plot([i - .5 for i in range(len(xLabels) + 1)], [y] * (len(xLabels) + 1), '--',
+                 lw=0.5, color='black', alpha=0.3)
 
-    # # Remove the tick marks; they are unnecessary with the tick lines we just
-    # # plotted.
-    # plt.tick_params(axis='both', which='both', bottom='off', top='off',
-    #                 labelbottom='on', left='off', right='off', labelleft='on')
+    # Remove the tick marks; they are unnecessary with the tick lines we just
+    # plotted.
+    plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                    labelbottom='on', left='off', right='off', labelleft='on')
 
     # Now that the plot is prepared, it's time to actually plot the data!
     # Note that I plotted the majors in order of the highest % in the final year.
@@ -74,25 +113,29 @@ def drawLineGraph(data, yAxis='week', categories=['data'], numWeeks=13, xLabel='
     #              'Math and Statistics': 0.75, 'Architecture': -0.75,
     #              'Computer Science': 0.75, 'Engineering': -0.25}
 
+    y_offsets = {('RF', 'regression'): .5, ('LReg', 'regression'): -.5}
+
     for rank, column in enumerate(categories):
         # Plot each line separately with its own color.
-        column_rec_name = column.replace('\n', '_').replace(' ', '_').lower()
+        #column_rec_name = column#.replace('\n', '_').replace(' ', '_').lower()
 
-        line = plt.plot(data[yAxis],
-                        data[column_rec_name],
+        line = plt.plot(range(len(xLabels)),
+                        data[column],
                         lw=2.5,
-                        color=color_sequence[rank])
+                        color=color_sequence[rank],
+                        marker='o',
+                        markersize=20)
 
         # Add a text label to the right end of every line. Most of the code below
         # is adding specific offsets y position because some labels overlapped.
-        y_pos = data[column_rec_name][-1] - 0.5
+        y_pos = data[column][-1]
 
-        # if column in y_offsets:
-        #     y_pos += y_offsets[column]
+        if column in y_offsets:
+            y_pos += y_offsets[column]
 
         # Again, make sure that all labels are large enough to be easily read
         # by the viewer.
-        plt.text(numWeeks+.5, y_pos, column, fontsize=14, color=color_sequence[rank])
+        plt.text(len(xLabels)-.5, y_pos, LABELS[column[0]] + column[1], fontsize=18, color=color_sequence[rank])
 
     # Make the title big enough so it spans the entire plot, but don't make it
     # so big that it requires two lines to show.
@@ -112,6 +155,9 @@ def drawLineGraph(data, yAxis='week', categories=['data'], numWeeks=13, xLabel='
 
 # data={'week': range(13), 'data': [5.3 for i in range(13)]}
 # drawLineGraph(data)
+
+#drawAccuracyGraph()
+drawMeanSquaresError()
 
 
 
